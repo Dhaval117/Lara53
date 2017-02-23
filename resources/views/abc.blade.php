f441406e2bbb90446a523399bfedce3bea155fd1
Dhaval117 <dhaval241195@gmail.com>
2017-02-23 21:48:11 +0530

    Better menu

--- resources/views/abc.blade.php
+++ resources/views/abc.blade.php
@@ -1,7 +1,55 @@
+@extends('layouts.master')
 
-@foreach(Cart::content() as $row)
-{{$row->name}}
-{{$row->price}}
-{{$row->qty}}
-<br/>
-@endforeach
+@section('title')
+One
+@endsection
+
+@section('content')
+<?php $cart = Cart::content(); ?>
+<div class="table-responsive cart_info">
+            @if(count($cart))
+            <table class="table table-condensed">
+                <thead>
+                    <tr class="cart_menu">
+                   
+                        <td class="description">Name</td>
+                        <td class="price">Price</td>
+                        <td class="quantity">Quantity</td>
+                        <td class="total">Total</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($cart as $item)
+                    <tr>
+                       
+                        <td class="cart_description">
+                            
+                            <p>{{$item->name}}</p>
+                        </td>
+                        <td class="cart_price">
+                            <p>${{$item->price}}</p>
+                        </td>
+                        <td class="cart_quantity">
+                            <div class="cart_quantity_button">
+                                <a class="cart_quantity_up" href=""> + </a>
+                                <input class="cart_quantity_input" type="text" name="quantity" value="{{$item->qty}}" autocomplete="off" size="2">
+                                <a class="cart_quantity_down" href=""> - </a>
+                            </div>
+                        </td>
+                        <td class="cart_total">
+                            <p class="cart_total_price">${{$item->subtotal}}</p>
+                        </td>
+                        <td class="cart_delete">
+                            <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                    @else
+                <p>You have no items in the shopping cart</p>
+                @endif
+                </tbody>
+			</table>
+			{{Cart::subtotal()}}
+        </div>
+ @endsection
